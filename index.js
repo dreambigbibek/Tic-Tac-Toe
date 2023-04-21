84d2b4a77ae13634247a47dfd2e42eade88be556
dreambigbibek <bibekgautam1997@gmail.com>
2023-04-21 18:43:25 +0545

    double click problem solved

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-console.log('hi')
 let clickPoint="X"
 gameOver=false
 function changeText(clickedId) {
@@ -7,13 +6,19 @@ function changeText(clickedId) {
         alert("Game over")
         return null;
     }
+    
     var element = document.getElementById(clickedId);
-      element.innerHTML = clickPoint;
-      checkWinner(clickPoint);
-      clickPoint=="X"?clickPoint="0":clickPoint="X"
-
-    // checkWinner(clickPoint);
+    if(element.innerHTML==''){
+        element.innerHTML = clickPoint;
+        checkWinner(clickPoint);
+        clickPoint=="X"?clickPoint="0":clickPoint="X"
+  
+      // checkWinner(clickPoint);
+    }
+    
   }
+
+
 
 let winningSequence=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 function checkWinner(clickPoint){
