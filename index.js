e65020abbf9b7279e1b381aad958404ca0009ac1
dreambigbibek <bibekgautam1997@gmail.com>
2023-04-21 18:16:15 +0545

    game Logic Added

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-// document.getElementsByClassName("clickDiv").innerHTML="0";
+
 console.log('hi')
 let clickPoint="X"
+gameOver=false
 function changeText(clickedId) {
-    var elements = document.getElementById(clickedId);
-      elements.innerHTML = clickPoint;
+    if(gameOver==true){
+        alert("Game over")
+        return null;
+    }
+    var element = document.getElementById(clickedId);
+      element.innerHTML = clickPoint;
+      checkWinner(clickPoint);
       clickPoint=="X"?clickPoint="0":clickPoint="X"
 
-    console.log("clicked");
+    // checkWinner(clickPoint);
   }
+
+let winningSequence=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+function checkWinner(clickPoint){
+     winningSequence.map((item,id)=>{
+        let filteredList=item.filter((itemInside,idInside)=>{
+           
+            
+           return((document.getElementById(itemInside.toString()).innerHTML==clickPoint)?(itemInside): (false)) ;
+        
+        })
+        // debugger;
+        if(filteredList.length==3)
+        {
+            gameOver=true;
+            document.getElementById("displayWinner").innerHTML=`winner is ${clickPoint}`;
+        }
+
+     })
+}
